3d7c72f3c673b23801865f068cb30e3f9928a5f4
Pol <romeropol31@gmail.com>
2023-11-02 00:10:45 +0100

    commit everything

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<style lang="scss">
+.home{
+  height: 100vh;
+}
+</style>
+<template>
+  <div class="home">
+    <navigation />
+  </div>
+</template>
+
+<script>
+import Navigation from './Navigation.vue';
+export default {
+  name: 'HomeView',
+  components: {Navigation}
+}
+</script>
